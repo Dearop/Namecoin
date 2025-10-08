7c308258da9bb13025538f909211d9d20c349261
Dearop <rocamar564p@gmail.com>
2025-10-08 11:20:56 +0200

    Fix lint

--- peer/impl/impl.go
+++ peer/impl/impl.go
@@ -245,38 +245,72 @@ func (n *node) listenLoop() {
 	}
 	var errorCount int
 	defer n.wg.Done()
+
 	for {
-		select {
-		case <-n.stopCh:
+		if n.shouldStop() {
 			return
-		default:
 		}
+
 		pkt, err := n.conf.Socket.Recv(time.Second * 1)
 		if err != nil {
-			if xerrors.Is(err, transport.TimeoutError(0)) {
-				continue
-			}
-			errorCount++
-			if errorCount > 10 {
-				// stop the node in a goroutine to avoid blocking the receive loop
-				go func() {
-					_ = n.Stop()
-				}()
-				if os.Getenv("GLOG") != "no" {
-					log.Printf("High error count, stopping node")
-				}
+			if n.handleSocketError(err, &errorCount) {
 				return
-			}
-			// Log errors but don't let them stop the node immediately
-			if os.Getenv("GLOG") != "no" && errorCount <= 5 {
-				log.Printf("Socket error (attempt %d): %v", errorCount, err)
 			}
 			continue
 		}
-		if errorCount > 0 {
-			errorCount = 0
-		}
+
+		n.resetErrorCount(&errorCount)
 		n.processPacket(pkt)
+	}
+}
+
+// shouldStop checks if the node should stop and returns true if it should
+func (n *node) shouldStop() bool {
+	select {
+	case <-n.stopCh:
+		return true
+	default:
+		return false
+	}
+}
+
+// handleSocketError handles socket errors and returns true if the node should stop
+func (n *node) handleSocketError(err error, errorCount *int) bool {
+	if xerrors.Is(err, transport.TimeoutError(0)) {
+		return false
+	}
+
+	*errorCount++
+	if *errorCount > 10 {
+		n.stopNodeOnHighErrorCount()
+		return true
+	}
+
+	n.logSocketError(*errorCount, err)
+	return false
+}
+
+// stopNodeOnHighErrorCount stops the node when error count is too high
+func (n *node) stopNodeOnHighErrorCount() {
+	go func() {
+		_ = n.Stop()
+	}()
+	if os.Getenv("GLOG") != "no" {
+		log.Printf("High error count, stopping node")
+	}
+}
+
+// logSocketError logs socket errors if appropriate
+func (n *node) logSocketError(errorCount int, err error) {
+	if os.Getenv("GLOG") != "no" && errorCount <= 5 {
+		log.Printf("Socket error (attempt %d): %v", errorCount, err)
+	}
+}
+
+// resetErrorCount resets the error count when a successful packet is received
+func (n *node) resetErrorCount(errorCount *int) {
+	if *errorCount > 0 {
+		*errorCount = 0
 	}
 }
 
